be83011f28268741b6d9a09f4625cb2311ecddb8
wiztigers <regis.witz@free.fr>
2017-01-11 19:39:24 +0100

    Roll-a-Ball tutorial 1.1, 1.2 and 2.1

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float Speed;
+
+	private Rigidbody body;
+
+	// Use this for initialization
+	void Start() {
+		body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+//	void Update() { }
+
+	void FixedUpdate() {
+		float hMove = Input.GetAxis("Horizontal");
+		float vMove = Input.GetAxis("Vertical");
+		Vector3 move = new Vector3(hMove, 0.0f, vMove);
+		body.AddForce(move * Speed);
+	}
+}
