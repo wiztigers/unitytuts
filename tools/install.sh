0faa5ce4171cd0a06efdb7078602abc884d7845f
wiztigers <regis.witz@free.fr>
2017-01-12 20:29:01 +0100

    okay ; starti travis-ci build analysis

--- tools/install.sh
+++ tools/install.sh
@@ -4,6 +4,8 @@ url="http://beta.unity3d.com/download/7083239589/unity-4.6.9.dmg"
 package="unity.dmg"
 echo Downloading from $url ...
 curl -o $package $url
+echo Done: `ls $package`
 
 echo Installing $package ...
 sudo installer -dumplog -package $package -target /
+echo Done
