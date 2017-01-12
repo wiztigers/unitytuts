ddd62d8804e2dc501db91d3f344b4269cdd3be20
wiztigers <regis.witz@free.fr>
2017-01-12 12:21:32 +0100

    Roll-a-ball tutorial 3.1

---
+++ Assets/scripts/Rotator.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	// we don't use physics, so use Update instead of FixedUpdate
+	void Update() {
+		transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+}
