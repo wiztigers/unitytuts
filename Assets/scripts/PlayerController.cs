17ab3e4ba54fb7415228b0132a126de76f6fa57d
wiztigers <regis.witz@free.fr>
2017-01-12 16:07:13 +0100

    Roll-a-ball tutorial 3.3

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,12 +4,28 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float Speed;
+	/// <summary>TODO: gameplay logic shouldn't be known by player</summary>
+	public UnityEngine.UI.Text Text;
 
 	private Rigidbody body;
+	private int count;
+	private int Score {
+		get { return count; }
+		set {
+			count = value;
+			if (count >= 12) {
+				Text.text = "Game Over";
+				Text.rectTransform.anchorMin = new Vector2(.5f,.5f);
+				Text.rectTransform.anchorMax = new Vector2(.5f,.5f);
+				Text.rectTransform.pivot     = new Vector2(.5f,.5f);
+			}
+			else Text.text = "Score: "+count;
+		}
+	}
 
 	void Start() {
 		body = GetComponent<Rigidbody>();
-		Debug.Log("player:start");
+		Score = 0;
 	}
 	
 	// Update is called once per frame
@@ -23,8 +39,8 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
-		Debug.Log("player:collide");
 		if (other.gameObject.CompareTag("PickUp")) {
+			Score++;
 			other.gameObject.SetActive(false);
 			//Destroy(other.gameObject);
 		}
