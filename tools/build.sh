f28da6d187c7bf49c6deb0fcab5b2e07f2fd2b7b
wiztigers <regis.witz@free.fr>
2017-01-13 08:22:03 +0100

    disable Linux build because I want to learn unity build log format.

--- tools/build.sh
+++ tools/build.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-echo "Building ..."
-find /Applications/Unity -type f -executable
 
 builder="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
 project="Roll-a-ball"
 logfile="unity.log"
 
-echo "Build $project for Windows ..."
+echo -n "Build $project for Windows ..."
 $builder -batchmode -nographics -silent-crashes \
 -projectPath $(pwd) \
 -logFile $(pwd)/$logfile \
 -buildWindowsPlayer "$(pwd)/bin/windows/$project.exe" \
 -quit
+echo "Done."
 
-echo "Build $project for Linux ..."
-$builder -batchmode -nographics -silent-crashes \
--projectPath $(pwd) \
--logFile $(pwd)/$logfile \
--buildLinuxUniversalPlayer "$(pwd)/bin/linux/$project.sh" \
--quit
+#echo -n "Build $project for Linux ..."
+#$builder -batchmode -nographics -silent-crashes \
+#-projectPath $(pwd) \
+#-logFile $(pwd)/$logfile \
+#-buildLinuxUniversalPlayer "$(pwd)/bin/linux/$project.sh" \
+#-quit
+#echo "Done."
 
 echo 'Build log:'
 cat $(pwd)/unity.log
--- tools/install.sh
+++ tools/install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-url="http://beta.unity3d.com/download/7083239589/unity-4.6.9.dmg"
+#url="http://beta.unity3d.com/download/7083239589/unity-4.6.9.dmg"
+#package="unity.dmg"
+url="http://netstorage.unity3d.com/unity/649f48bbbf0f/MacEditorInstaller/Unity-5.4.1f1.pkg"
 package="unity.pkg"
 echo Downloading from $url ...
 curl -o $package $url
