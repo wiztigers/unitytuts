f14864c5dfe1383d8d64d01e75c5da15fa11c073
wiztigers <regis.witz@free.fr>
2017-01-15 12:06:40 +0100

    improve install script and upgrade to last unity version

--- tools/build.sh
+++ tools/build.sh
@@ -5,21 +5,21 @@ builder="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
 project="Roll-a-ball"
 logfile="unity.log"
 
-echo -n "Build $project for Windows ..."
-$builder -batchmode -nographics -silent-crashes \
--projectPath $(pwd) \
--logFile $(pwd)/$logfile \
--buildWindowsPlayer "$(pwd)/bin/windows/$project.exe" \
--quit
-echo "Done."
-
-#echo -n "Build $project for Linux ..."
+#echo -n "Build $project for Windows ..."
 #$builder -batchmode -nographics -silent-crashes \
 #-projectPath $(pwd) \
 #-logFile $(pwd)/$logfile \
-#-buildLinuxUniversalPlayer "$(pwd)/bin/linux/$project.sh" \
+#-buildWindowsPlayer "$(pwd)/bin/windows/$project.exe" \
 #-quit
 #echo "Done."
+
+echo -n "Build $project for Linux ..."
+$builder -batchmode -nographics -silent-crashes \
+-projectPath $(pwd) \
+-logFile $(pwd)/$logfile \
+-buildLinuxUniversalPlayer "$(pwd)/bin/linux/$project.sh" \
+-quit
+echo "Done."
 
 echo 'Build log:'
 cat $(pwd)/unity.log
--- tools/install.sh
+++ tools/install.sh
@@ -1,13 +1,67 @@
 #!/bin/sh
 
-#url="http://beta.unity3d.com/download/7083239589/unity-4.6.9.dmg"
-#package="unity.dmg"
-url="http://netstorage.unity3d.com/unity/649f48bbbf0f/MacEditorInstaller/Unity-5.4.1f1.pkg"
-package="unity.pkg"
-echo Downloading from $url ...
-curl -o $package $url
-echo Done: `ls $package`
+# https://support.unity3d.com/hc/en-us/articles/211013503-How-can-I-have-a-fully-automated-build-process-that-can-fetch-and-install-any-version-of-Unity-into-our-build-environment-
+#
+#For Windows:
+#http://download.unity3d.com/download_unity/{Changeset}/Windows64EditorInstaller/UnitySetup64.exe
+#http://download.unity3d.com/download_unity/{Changeset}/WindowsDocumentationInstaller/UnityDocumentationSetup.exe
+#http://download.unity3d.com/download_unity/{Changeset}/WindowsDevelopmentWebPlayerInstaller/UnityWebPlayerDevelopment.exe
+#http://download.unity3d.com/download_unity/{Changeset}/WindowsStandardAssetsInstaller/UnityStandardAssetsSetup.exe
+#http://download.unity3d.com/download_unity/{Changeset}/WindowsExampleProjectInstaller/UnityExampleProjectSetup.exe
+#http://download.unity3d.com/download_unity/{Changeset}/TargetSupportInstaller/UnitySetup-Windows-Support-for-Editor-{version}.exe
+#http://download.unity3d.com/download_unity/{Changeset}/TargetSupportInstaller/UnitySetup-Android-Support-for-Editor-{version}.exe
+#http://download.unity3d.com/download_unity/{Changeset}/TargetSupportInstaller/UnitySetup-iOS-Support-for-Editor-{version}.exe
+#http://download.unity3d.com/download_unity/{Changeset}/TargetSupportInstaller/UnitySetup-AppleTV-Support-for-Editor-{version}.exe
+#http://download.unity3d.com/download_unity/{Changeset}/TargetSupportInstaller/UnitySetup-Linux-Support-for-Editor-{version}.exe
+#http://download.unity3d.com/download_unity/{Changeset}/TargetSupportInstaller/UnitySetup-Mac-Support-for-Editor-{version}.exe
+#http://download.unity3d.com/download_unity/{Changeset}/TargetSupportInstaller/UnitySetup-Metro-Support-for-Editor-{version}.exe
+#http://download.unity3d.com/download_unity/{Changeset}/TargetSupportInstaller/UnitySetup-UWP-IL2CPP-Support-for-Editor-{version}.exe
+#http://download.unity3d.com/download_unity/{Changeset}/TargetSupportInstaller/UnitySetup-Samsung-TV-Support-for-Editor-{version}.exe
+#http://download.unity3d.com/download_unity/{Changeset}/TargetSupportInstaller/UnitySetup-Tizen-Support-for-Editor-{version}.exe
+#http://download.unity3d.com/download_unity/{Changeset}/TargetSupportInstaller/UnitySetup-WebGL-Support-for-Editor-{version}.exe
+#
+#For OSX:
+#http://download.unity3d.com/download_unity/{Changeset}/MacEditorInstaller/Unity.pkg
+#http://download.unity3d.com/download_unity/{Changeset}/MacDocumentationInstaller/Documentation.pkg
+#http://download.unity3d.com/download_unity/{Changeset}/MacWebPlayerInstaller/WebPlayer.pkg
+#http://download.unity3d.com/download_unity/{Changeset}/MacStandardAssetsInstaller/StandardAssets.pkg
+#http://download.unity3d.com/download_unity/{Changeset}/MacExampleProjectInstaller/Examples.pkg
+#http://download.unity3d.com/download_unity/{Changeset}/MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-{version}.pkg
+#http://download.unity3d.com/download_unity/{Changeset}/MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-{version}.pkg
+#http://download.unity3d.com/download_unity/{Changeset}/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-{version}.pkg
+#http://download.unity3d.com/download_unity/{Changeset}/MacEditorTargetInstaller/UnitySetup-AppleTV-Support-for-Editor-{version}.pkg
+#http://download.unity3d.com/download_unity/{Changeset}/MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-{version}.pkg
+#http://download.unity3d.com/download_unity/{Changeset}/MacEditorTargetInstaller/UnitySetup-Samsung-TV-Support-for-Editor-{version}.pkg
+#http://download.unity3d.com/download_unity/{Changeset}/MacEditorTargetInstaller/UnitySetup-Tizen-Support-for-Editor-{version}.pkg
+#http://download.unity3d.com/download_unity/{Changeset}/MacEditorTargetInstaller/UnitySetup-WebGL-Support-for-Editor-{version}.pkg
+#http://download.unity3d.com/download_unity/{Changeset}/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-{version}.pkg
 
-echo Installing $package ...
-sudo installer -dumplog -package $package -target /
-echo Done
+# $base_url/$changeset/unity-$version-$platform.ini
+# (where platform is 'osx' or 'win') has this info
+
+#base_url=http://netstorage.unity3d.com/unity
+base_url=http://download.unity3d.com/download_unity
+changeset=38b4efef76f0
+version=5.5.0f3
+
+download() {
+  file=$1
+  url="$base_rul/$changeset/$package"
+
+  echo "Download from $url:"
+  curl -o `basename "$package"` "$url"
+}
+
+install() {
+  package=$1
+  download "$package"
+
+  echo "Install "`basename "$package"`" ..."
+  sudo installer -dumplog -package `basename "$package"` -target /
+  echo "Done."
+}
+
+install "MacEditorInstaller/Unity.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$version.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$version.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$version.pkg"
