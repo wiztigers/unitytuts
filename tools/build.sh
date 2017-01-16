e0b454fba1cebf178f327bd2744d809c4d1d25cc
wiztigers <regis.witz@free.fr>
2017-01-16 14:18:55 +0100

    scrogneugneu spaces (of course that's the kind of error I wanted to run into)

--- tools/build.sh
+++ tools/build.sh
@@ -4,15 +4,15 @@ builder="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
 logfile="unity.log"
 
 build() {
-  project=$1
+  project="$1"
   pushd "$project"
   target=$2
   option=$3
   echo "Build project:$project for target:$target ..."
   $builder -batchmode -nographics -silent-crashes \
-    -projectPath $(pwd) \
-    -logFile $(pwd)/$logfile \
-    -$option "$(pwd)/bin/$target/$project.sh" \
+    -projectPath "$(pwd)" \
+    -logFile "$logfile" \
+    -$option "bin/$target/$project.sh" \
     -quit
   echo "Create artefact \"$project-$target.zip\" ..."
   pushd bin/$target && zip -r "../$project-$target.zip" . && popd
